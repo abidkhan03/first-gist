50468dd5f48686166ff54740697e9ceeb5772e68
Abdullah Abid <69511228+abidkhan03@users.noreply.github.com>
2023-01-20 15:14:46 +0500

    Create git-commands.sh

---
+++ git-commands.sh
@@ -0,0 +1,40 @@
+# Check Version of git installed
+git --version
+
+# Add Global Name to git
+git config --global user.name "abidkhan03"
+
+# Add Global Email id to git
+git config --global user.email "abidkhan42935@gmail.com"
+
+# Check if names are added
+git config --list
+
+# Get Help with configurations
+git help config   / git config --help
+
+
+# check status of current git repository 
+git status
+
+# add git file
+git init 
+
+# Check if status updated
+git status
+
+# List all files (Including Hidden Files)
+ls -la
+
+# Remove Directory
+rm -rf .git
+
+# Add files and folders to be ignored by Git 
+touch .gitignore
+
+# Add Specific file
+git add .gitignore
+
+# Add All Files in current Directory
+git add -A
+
